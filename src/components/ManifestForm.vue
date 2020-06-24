4ff639029329753c0b51609b0e351726d8642a8a
Hidenori Miyoshi <miyohide@gmail.com>
2020-06-24 19:05:49 +0900

    add buildpack

--- src/components/ManifestForm.vue
+++ src/components/ManifestForm.vue
@@ -6,6 +6,15 @@
         <input v-model="appName" id="appName" type="text" class="form-control">
       </div>
       <div class="form-group">
+        <label for="buildpack">実行環境（ビルドパック）</label>
+        <select v-model="buildpack" name="buildpack" id="buildpack" class="form-control">
+          <option disabled value="">選択してください</option>
+          <option>Java</option>
+          <option>Ruby</option>
+          <option>その他</option>
+        </select>
+      </div>
+      <div class="form-group">
         <label for="manifest">manifest.yml</label>
         <textarea v-model="manifest" name="manifest" id="manifest" cols="30" rows="10" class="form-control" readonly></textarea>
       </div>
@@ -18,13 +27,32 @@ export default {
   name: 'ManifestForm',
   data: function () {
     return {
-      appName: ''
+      appName: '',
+      buildpack: ''
     }
   },
   computed: {
     manifest: function () {
-      return "---\napplications:\n- name: " + this.appName
+      let mfList = []
+      mfList = mfList.concat(init(this.appName))
+      mfList = mfList.concat(buildpack_str(this.buildpack))
+      return mfList.join("\n")
     }
   }
+}
+
+function init (appName) {
+  let rval = []
+  rval.push("---")
+  rval.push("applications:")
+  rval.push("- name: " + appName)
+  return rval
+}
+
+function buildpack_str (buildpack) {
+  let rval = []
+  rval.push("  buildpacks:")
+  rval.push("    - " + buildpack)
+  return rval
 }
 </script>
