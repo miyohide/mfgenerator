2c8be842c20b1c493105e153353dd1741001afd9
Hidenori Miyoshi <miyohide@gmail.com>
2020-06-24 18:53:50 +0900

    add textarea.

--- src/components/ManifestForm.vue
+++ src/components/ManifestForm.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
+  <div class="form-container container">
+    <form>
+      <div class="form-group">
+        <label for="appName" class="col-form-label col-sm-2">名前</label>
+        <input v-model="appName" id="appName" type="text" class="form-control">
+      </div>
+      <div class="form-group">
+        <textarea v-model="manifest" name="manifest" id="" cols="30" rows="10" class="form-control" readonly></textarea>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ManifestForm',
+  data: function () {
+    return {
+      appName: ''
+    }
+  },
+  computed: {
+    manifest: function () {
+      return "---\napplications:\n- name: " + this.appName
+    }
+  }
 }
 </script>
