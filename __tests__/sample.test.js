4fdf7473eff7f301141642553d3bcbafe8964ace
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-01 17:39:34 +0900

    Revert "create first test."
    This reverts commit dfa5caf04b3ed455659ccc03747ad28bffce60c1.

--- __tests__/sample.test.js
+++
@@ -1,9 +0,0 @@
-import {mount} from '@vue/test-utils'
-import Component from '@/App'
-
-describe('ManifestForm', () => {
-  it('is a Vue instance', () => {
-    const wrapper = mount(Component)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
