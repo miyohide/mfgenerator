3aa6bd466cc169a2ca8e4c9d9fd7e550d0eab22e
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-01 20:32:23 +0900

    use before/afterEach

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,8 +2,17 @@ import { shallowMount } from '@vue/test-utils'
 import ManifestForm from '@/components/ManifestForm.vue'
 
 describe('ManifestForm.vue', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(ManifestForm)
+  })
+
+  afterEach(() => {
+    wrapper.destroy()
+  })
+
   it('renders application name', () => {
-    const wrapper = shallowMount(ManifestForm)
     wrapper.find('#appName').setValue('applicationName')
     expect(wrapper.vm.manifest).toContain('- name: applicationName')
   })
