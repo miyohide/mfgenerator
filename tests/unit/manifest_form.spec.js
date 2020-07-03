93c8b38639a165b21c0d8e4c984beecadf075148
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-03 18:50:47 +0900

    add a test that click addEnv button

--- tests/unit/manifest_form.spec.js
+++ tests/unit/manifest_form.spec.js
@@ -34,4 +34,15 @@ describe('ManifestForm.vue', () => {
     expect(wrapper.vm.manifest).toContain('  env:')
     expect(wrapper.vm.manifest).toContain('    env_key_0: env_val_0')
   })
+
+  it('renders new env inputs when addEnv button click', async () => {
+    wrapper.find('#addEnv').trigger('click')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.html()).toContain('環境変数1のキー')
+    expect(wrapper.html()).toContain('環境変数1の値')
+    wrapper.find('#envKey1').setValue('env_key_1')
+    wrapper.find('#envVal1').setValue('env_val_1')
+    expect(wrapper.vm.manifest).toContain('  env:')
+    expect(wrapper.vm.manifest).toContain('    env_key_1: env_val_1')
+  })
 })
